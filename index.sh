d7cc9383bb83b9844b77fc78d239ccbecc88aa71
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2019-11-19 11:09:20 +0900

    Temporary fix for colors

--- index.sh
+++ index.sh
@@ -3,24 +3,24 @@ set -euo pipefail
 
 # Use colors, but only if connected to a terminal, 
 # and that terminal supports colors.
-if which tput >/dev/null 2>&1; then
-    ncolors=$(tput colors)
-fi
-if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-  RED="$(tput setaf 1)"
-  GREEN="$(tput setaf 2)"
-  YELLOW="$(tput setaf 3)"
-  BLUE="$(tput setaf 4)"
-  BOLD="$(tput bold)"
-  NORMAL="$(tput sgr0)"
-else
+#if which tput >/dev/null 2>&1; then
+#    ncolors=$(tput colors)
+#fi
+#if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+#  RED="$(tput setaf 1)"
+#  GREEN="$(tput setaf 2)"
+#  YELLOW="$(tput setaf 3)"
+#  BLUE="$(tput setaf 4)"
+#  BOLD="$(tput bold)"
+#  NORMAL="$(tput sgr0)"
+#else
   RED=""
   GREEN=""
   YELLOW=""
   BLUE=""
   BOLD=""
   NORMAL=""
-fi
+#fi
 
 # Check if `zsh` is present.
 if command -v zsh >/dev/null 2>&1; then
